436dc3896f1fa417eeafbef0bc7075e9c759285f
Aaron Haughton <dreamcast2000@outlook.com>
2020-08-21 09:42:19 -0400

    wired up IBOutlets

---
+++ MortgageCalculator/Controllers/MapkitHelper.swift
@@ -0,0 +1,13 @@
+//
+//  MapkitHelper.swift
+//  MortgageCalculator
+//
+//  Created by Joe on 8/21/20.
+//  Copyright © 2020 Kevin Stewart. All rights reserved.
+//
+
+import Foundation
+
+class MapkitHelper {
+    
+}
