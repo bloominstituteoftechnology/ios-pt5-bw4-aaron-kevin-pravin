a86d19f116a7cbbf8675cb9817974fea61dd82c6
Aaron Haughton <dreamcast2000@outlook.com>
2020-08-24 23:35:42 -0400

    created TableView Controller and added segue

--- MortgageCalculator/View Controllers/HomeTableViewController.swift
+++ MortgageCalculator/View Controllers/HomeTableViewController.swift
@@ -15,7 +15,16 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+<<<<<<< HEAD
 
+=======
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+>>>>>>> created TableView Controller and added segue
     }
     
     // MARK: - Table view data source
@@ -46,8 +55,28 @@ class HomeTableViewController: UITableViewController {
      }
      }
      */
+<<<<<<< HEAD
 
      
+=======
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+>>>>>>> created TableView Controller and added segue
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
