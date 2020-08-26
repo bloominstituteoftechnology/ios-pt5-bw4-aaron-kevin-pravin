039d33a50fb81d0427e228f1bda3a099d04efc28
Aaron Haughton <dreamcast2000@outlook.com>
2020-08-25 21:23:14 -0400

    Added Save IBAction in mortgage Controller.

--- MortgageCalculator/View Controllers/HomeTableViewController.swift
+++ MortgageCalculator/View Controllers/HomeTableViewController.swift
@@ -15,12 +15,6 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
@@ -40,33 +34,18 @@ class HomeTableViewController: UITableViewController {
         return cell
     }
     
-    /*
+
      // Override to support editing the table view.
      override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
      if editingStyle == .delete {
-     // Delete the row from the data source
+        mortgageController.saveToPersistentStore()
+        mortgageController.loadFromPersistentStore()
+        tableView.reloadData()
      tableView.deleteRows(at: [indexPath], with: .fade)
      } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        
      }
      }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
     
     // MARK: - Navigation
     
