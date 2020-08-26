1d45ef6692892a26b1b7b3b07652cc9e8ab3bb31
Aaron Haughton <dreamcast2000@outlook.com>
2020-08-25 21:23:14 -0400

    Added Save IBAction in mortgage Controller.

--- MortgageCalculator/View Controllers/HomeTableViewController.swift
+++ MortgageCalculator/View Controllers/HomeTableViewController.swift
@@ -15,16 +15,7 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
 
-=======
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
->>>>>>> created TableView Controller and added segue
     }
     
     // MARK: - Table view data source
@@ -44,39 +35,20 @@ class HomeTableViewController: UITableViewController {
         return cell
     }
     
-    /*
+
      // Override to support editing the table view.
      override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
      if editingStyle == .delete {
-     // Delete the row from the data source
+        mortgageController.saveToPersistentStore()
+        mortgageController.loadFromPersistentStore()
+        tableView.reloadData()
      tableView.deleteRows(at: [indexPath], with: .fade)
      } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-<<<<<<< HEAD
 
-     
-=======
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
      }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
     
     
->>>>>>> created TableView Controller and added segue
+
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
